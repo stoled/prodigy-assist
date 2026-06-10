fee13121a9b044854df9d799594cc9082b202fe1
Grigorii <grigoriiv@slap-star.com>
2026-06-10 23:53:04 +0400

    refactor: migrate to prisma 7

--- backend/dist/prisma.config.d.ts
+++
@@ -1,3 +0,0 @@
-import './load-root-env';
-declare const _default: import("@prisma/config").PrismaConfigInternal;
-export default _default;
--- backend/dist/src/messages/dto/send-message.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class SendMessageDto {
-    telegramId: string;
-    text: string;
-}
